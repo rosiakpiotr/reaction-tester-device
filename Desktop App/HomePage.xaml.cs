c066584dc7d01f322bdf498f548dd8ee903dd2ce
Piotrek <rosiakpiotr3@gmail.com>
2020-05-28 16:07:36 +0200

    Moved files.

---
+++ Desktop App/HomePage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RCClient
+{
+    public partial class HomePage : Page
+    {
+        private ConnectionManager connectionManager;
+
+        public HomePage(ConnectionManager connectionManager)
+        {
+            InitializeComponent();
+
+            this.connectionManager = connectionManager;
+
+            // Set callbacks, what should happen when arduino connected / disconnected.
+            connectionManager.ConnectedCallback = () =>
+            {
+                // This executed when arduino gets plugged in.
+                //Trace.WriteLine("Arduino connected");
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    GreenDotImage.Visibility = Visibility.Visible;
+                    RedDotImage.Visibility = Visibility.Hidden;
+                }));
+            };
+
+            connectionManager.DisconnectedCallback = () =>
+            {
+
+                // This executed when arduino gets unplugged.
+                //Trace.WriteLine("Arduino DC");
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    GreenDotImage.Visibility = Visibility.Hidden;
+                    RedDotImage.Visibility = Visibility.Visible;
+                }));
+            };
+
+            // Check for connection to show red or green indicator.
+            connectionManager.Refresh();
+        }
+
+        private void OnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // Start new page showing settings.
+            NavigationService.Navigate(new AboutPage());
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Start new page with pre settings to game.
+            NavigationService.Navigate(new PreGameSettingsPage(connectionManager));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Reaction tester was developed in two versions. First is desktop game, present in folder 'reaction-tester' and second - psychical device that works itself with its display or with computer app located in 'device/Desktop App'.
 
-Further explains available in Polish language under '/device/opis techniczny.txt'
+Further explains available in Polish language at opis techniczny.txt
 
